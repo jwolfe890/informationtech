f70e167377a94892ef2969b42493777f277c15fa
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-02-16 13:33:49 -0500

    changed image

--- src/containers/gallery.js
+++ src/containers/gallery.js
@@ -17,8 +17,8 @@ class Gallery extends React.Component {
     return (
       <div className="container">
         <Slider {...settings} arrows={true} >
-          <div className="imtxt"><img src='https://preview.ibb.co/fuL4BS/learn_fast.jpg' /><div className="nm"><span><h1>Fast Learner</h1><p>I'm a fast learner and excited to learn new things.</p></span></div></div> 
           <div className="imtxt"><img src='https://preview.ibb.co/fhj35n/Webp_net_resizeimage.jpg' /><div className="nm"><span class="spn"><h1>Awesome Skills</h1><p>I love to code, and I'm great at it.</p></span></div></div>
+          <div className="imtxt"><img src='https://preview.ibb.co/fuL4BS/learn_fast.jpg' /><div className="nm"><span><h1>Fast Learner</h1><p>I'm a fast learner and excited to learn new things.</p></span></div></div> 
           <div className="imtxt"><img src='https://preview.ibb.co/dpZWrS/Programming.jpg' /><div className="nm"><span><h1>Works Hard</h1><p>I don't give up until I've figured it out.</p></span></div></div>
         </Slider>
       </div>
