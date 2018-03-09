6ab27f011208aef587443617178e33ec6c1b3eca
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-03-09 02:15:52 -0500

    top footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,10 @@ import './footer.css'
 const Footer = () => {
     return (
         <div class="wrapper">
+            <div class="wrapper2 top">
+                <div class="item">CONTACT</div>
+                <div class="social2 item2">Email me at: <a class="em" href="mailto:jwolfe890@gmail.com?">jwolfe890@gmail.com</a></div>
+            </div>
 
         </div>
     );
