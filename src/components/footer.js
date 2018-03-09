35182964a736cd863ca83e5805d0905ffb4eeda5
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-03-09 03:07:39 -0500

    all

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             </div>
             <div class="wrapper2 bottom">
                 <div class="item">FOLLOW</div>
-                <div class="social2 item2">I'm all over social media. Check me out on <a href="https://github.com/jwolfe890">Github</a>, <a href="https://www.linkedin.com/in/johnmwolfe/">Linkedin</a>, <a href="https://twitter.com/johnwolfe820">Twitter</a>, and <a  href="https://medium.com/@johnwolfe820">Medium.</a></div>
+                <div class="social2 item2">Check me out on <a href="https://github.com/jwolfe890">Github</a>, <a href="https://www.linkedin.com/in/johnmwolfe/">Linkedin</a>, <a href="https://twitter.com/johnwolfe820">Twitter</a>, and <a  href="https://medium.com/@johnwolfe820">Medium.</a></div>
             </div>
         </div>
     );
