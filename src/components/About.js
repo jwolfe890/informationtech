0270833941f028f9ab3e1510606169cbd661f6a3
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-03-09 20:46:11 -0500

    all

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ const About = () => {
                     <span>Hello, I'm John! I'm a fullstack web developer with experience in JavaScript, React, Ruby on Rails, CSS, and Responsive Design. I bring strong skills in research, project management, and complex problems solving that helps innovative companies attract users and drive growth.</span> 
                         <br/>
                         <br/>
-                    <span>I work hard to stay on the cutting edge of software development, and have a talent for learning new technologies efficiently and effectively. Check out some of my projects below and don't hesitate to <a class="abt" href="mailto:jwolfe890@gmail.com?">contact me</a> for your next project!</span>
+                    <span>I work hard to stay on the cutting edge of software development, and I have a passion for learning new information. Check out some of my projects below and don't hesitate to <a class="abt" href="mailto:jwolfe890@gmail.com?">contact me</a> for your next project!</span>
                 </div>
             </div>
         </div>
