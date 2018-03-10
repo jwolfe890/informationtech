632c58562d41d5a6966646cb7c62d34c110cfce3
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-03-09 20:51:42 -0500

    all

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
                     <img className="john" src="http://res.cloudinary.com/dszmqm6le/image/upload/v1519519186/john.jpg" />
                 </div>
                 <div className="things2">
-                    <span>Hello, I'm John! I'm a fullstack web developer with experience in JavaScript, React, Ruby on Rails, CSS, and Responsive Design. I bring strong skills in research, project management, and complex problems solving that helps innovative companies attract users and drive growth.</span> 
+                    <span>Hello, I'm John! I'm a fullstack web developer with experience in JavaScript, React, Ruby on Rails, CSS, and Responsive Design. I bring strong skills in research, project management, and complex problems solving in JavaScript, React, and Rails. I aim to help innovative companies attract users and drive growth.</span> 
                         <br/>
                         <br/>
                     <span>I work hard to stay on the cutting edge of software development, and I have a passion for learning new information. Check out some of my projects below and don't hesitate to <a class="abt" href="mailto:jwolfe890@gmail.com?">contact me</a> for your next project!</span>
